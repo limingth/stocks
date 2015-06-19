6a1f3f4809bdc8bcbe1b3ea077a45f2f709897fb
Liu Jiyang <2411mail@gmail.com>
2015-06-19 17:34:38 +0800

    update seed

--- server/seeds.js
+++ server/seeds.js
@@ -1,39 +1,39 @@
 Meteor.startup(function() {
   var stocks = [
     {
-      code: '600230',
-      name: '沧州大化',
-      value: '-10%'
+      "code": '600230',
+      "name": '沧州大化',
+      "value": '-10%'
     },
     {
-      code: '600231',
-      name: '凌钢股份',
-      value: '-5%'
+      "code": '600231',
+      "name": '凌钢股份',
+      "value": '-5%'
     },
     {
-      code: '600232',
-      name: '金鹰股份',
-      value: '-0.5%'
+      "code": '600232',
+      "name": '金鹰股份',
+      "value": '-0.5%'
     },
     {
-      code: '600233',
-      name: '大杨创世',
-      value: '0.5%'
+      "code": '600233',
+      "name": '大杨创世',
+      "value": '0.5%'
     },
     {
-      code: '600234',
-      name: '山水文化',
-      value: '5%'
+      "code": '600234',
+      "name": '山水文化',
+      "value": '5%'
     },
     {
-      code: '600235',
-      name: '民丰特纸',
-      value: '10%'
+      "code": '600235',
+      "name": '民丰特纸',
+      "value": '10%'
     },
     {
-      code: '600236',
-      name: '桂冠电力',
-      value: '5%'
+      "code": '600236',
+      "name": '桂冠电力',
+      "value": '5%'
     },
     {
       "code":'600000',
